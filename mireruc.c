0009134ad50b2e9a8d51f17580db4fd7ab16ef64
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-14 18:24:34 +0900

    Add a button to quit
    TODO:
    
      * Add callback.

--- mireruc.c
+++ mireruc.c
@@ -3,12 +3,16 @@
 int
 main (int argc, char* argv[]) {
     GtkWidget *window;
+    GtkWidget *button;
 
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request(window, 300, 200);
 
-    gtk_widget_show(window);
+    button = gtk_button_new_with_label("Quit");
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    gtk_widget_show_all(window);
     gtk_main();
 
     return 0;
