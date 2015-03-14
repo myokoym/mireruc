448e9850960b13157227135e0b7798337ae8a32e
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-14 19:55:05 +0900

    Add a widget to show an image

--- mireruc.c
+++ mireruc.c
@@ -8,14 +8,22 @@ cb_button_clicked (GtkWidget *button, gpointer user_data) {
 int
 main (int argc, char* argv[]) {
     GtkWidget *window;
+    GtkWidget *vbox;
+    GtkWidget *image;
     GtkWidget *button;
 
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request(window, 300, 200);
 
+    vbox = gtk_vbox_new(FALSE, 2);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    image = gtk_image_new_from_file(argv[1]);
+    gtk_box_pack_start(GTK_BOX(vbox), image, TRUE, TRUE, 0);
+
     button = gtk_button_new_with_label("Quit");
-    gtk_container_add(GTK_CONTAINER(window), button);
+    gtk_box_pack_start(GTK_BOX(vbox), button, TRUE, TRUE, 0);
 
     g_signal_connect(G_OBJECT(button), "clicked",
                      G_CALLBACK(cb_button_clicked), NULL);
